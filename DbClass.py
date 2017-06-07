d23475f288f9e671e0c9815abdf784fca8b0f0a3
VincentVanbecelaere <vincent.vanbecelaere@student.howest.be>
2017-06-07 14:40:05 +0200

    Initial Commit

---
+++ DbClass.py
@@ -0,0 +1,43 @@
+class DbClass:
+    def __init__(self):
+        import mysql.connector as connector
+
+        self.__dsn = {
+            "host": "HostnameHere",
+            "user": "UserNameHere",
+            "passwd": "PasswdHere",
+            "db": "DBNameHere"
+        }
+
+        self.__connection = connector.connect(**self.__dsn)
+        self.__cursor = self.__connection.cursor()
+
+    def getDataFromDatabase(self):
+        # Query zonder parameters
+        sqlQuery = "SELECT * FROM tablename"
+        
+        self.__cursor.execute(sqlQuery)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def getDataFromDatabaseMetVoorwaarde(self, voorwaarde):
+        # Query met parameters
+        sqlQuery = "SELECT * FROM tablename WHERE columnname = '{param1}'"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=voorwaarde)
+        
+        self.__cursor.execute(sqlCommand)
+        result = self.__cursor.fetchall()
+        self.__cursor.close()
+        return result
+
+    def setDataToDatabase(self, value1):
+        # Query met parameters
+        sqlQuery = "INSERT INTO tablename (columnname) VALUES ('{param1}')"
+        # Combineren van de query en parameter
+        sqlCommand = sqlQuery.format(param1=value1)
+
+        self.__cursor.execute(sqlCommand)
+        self.__connection.commit()
+        self.__cursor.close()
