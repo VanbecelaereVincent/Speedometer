14b9e37750b0f589048dedad9c9669f242949866
VincentVanbecelaere <vincent.vanbecelaere@student.howest.be>
2017-06-08 10:07:22 +0200

    Dag 1

--- Speedometer.py
+++ Speedometer.py
@@ -1,12 +1,24 @@
 from flask import Flask
+from flask import render_template
+import os
 
 app = Flask(__name__)
 
 
 @app.route('/')
-def hello_world():
-    return 'Hello World!'
+def home():
+    return render_template('home.html')
+
+@app.route('/speedometer')
+def speedometer():
+    return render_template('speedometer.html')
+
+@app.route('/contact')
+def contact():
+    return render_template('contact.html')
+
 
 
 if __name__ == '__main__':
-    app.run()
+    port = int(os.environ.get("PORT", 8080))
+    app.run(host='0.0.0.0',port=8080, debug=True)
